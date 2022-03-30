f3baa3b304243ff774ca4b65b3d84d2cd0db139b
Magnus Lilja <magnus@connoisseur.se>
2022-03-30 20:26:56 +0200

    updated to resteasy-client 6 and refactoring

--- src/main/java/client/ClientService.java
+++ src/main/java/client/ClientService.java
@@ -1,28 +1,37 @@
 package client;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import model.Profile;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.Response;
+//import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+//import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+//import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+//
+//import javax.ws.rs.ProcessingException;
+//import javax.ws.rs.client.Entity;
+//import javax.ws.rs.core.GenericType;
+//import javax.ws.rs.core.Response;
 import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class ClientService {
     static String uri = "http://164.92.231.95:8080/Grupp1ServerApplication/api";
-    static ResteasyClient client = new ResteasyClientBuilder().build();
+    static Client client = ClientBuilder.newBuilder().build();
 
     static Profile addProfile(Profile newProfile) {
-
         try {
-            ResteasyWebTarget webTarget = client.target(uri + "/profiles");
-            Entity newProfileEntity = Entity.entity(newProfile, "application/JSON");
-            Response response = webTarget.request().buildPost(newProfileEntity).invoke();
-            return response.readEntity(Profile.class);
-        } catch (ProcessingException ex) {
+            return client.target(uri + "/profiles")
+                    .request()
+                    .buildPost(Entity.entity(newProfile, "application/JSON"))
+                    .invoke()
+                    .readEntity(Profile.class);
+        } catch (Exception ex) {
             System.err.println("This resource does not exist");
             return null;
         }
@@ -31,11 +40,11 @@ public class ClientService {
     static Profile getProfileById(int id) {
 
         try {
-            ResteasyWebTarget webTarget = client.target(uri + "/profiles/" + id);
+            WebTarget webTarget = client.target(uri + "/profiles/" + id);
             Response response = webTarget.request().get();
             Profile profile = response.readEntity(Profile.class);
             return profile;
-        } catch (ProcessingException ex) {
+        } catch (Exception ex) {
             System.err.println("This resource does not exist");
             return null;
         }
@@ -43,44 +52,50 @@ public class ClientService {
 
     static boolean deleteProfile(int id) {
         try {
-            ResteasyWebTarget webTarget = client.target(uri + "/profiles/" + id);
+            WebTarget webTarget = client.target(uri + "/profiles/" + id);
             Response response = webTarget.request().delete();
-            Profile profile = response.readEntity(Profile.class);
             if (response.getStatus() == 204) {
                 return true;
             }
             return false;
-        } catch (ProcessingException ex) {
+        } catch (Exception ex) {
             System.err.println("This resource does not exist");
             return false;
         }
     }
 
     static List<Profile> searchByLastAndFirstName(String lastName, String firstName) {
-        ResteasyWebTarget webTarget = client.target(uri + "/profiles/search?lastname=" + lastName + "&firstname=" + firstName);
-        Response response = webTarget.request().get();
-        return response.readEntity(new GenericType<List<Profile>>() {
+        return client.target(uri + "/profiles/search?lastname=" + lastName + "&firstname=" + firstName)
+                .request()
+                .get()
+                .readEntity(new GenericType<List<Profile>>() {
         });
     }
 
     static List<Profile> searchByLastname(String lastName) {
-        ResteasyWebTarget webTarget = client.target(uri + "/profiles/search?lastname=" + lastName);
-        Response response = webTarget.request().get();
-        return response.readEntity(new GenericType<List<Profile>>() {
+        return client.target(uri + "/profiles/search?lastname=" + lastName)
+                .request()
+                .get()
+                .readEntity(new GenericType<List<Profile>>() {
         });
     }
 
     static List<Profile> searchByFirstname(String firstName) {
-        ResteasyWebTarget webTarget = client.target(uri + "/profiles/search?firstname=" + firstName);
-        Response response = webTarget.request().get();
-        return response.readEntity(new GenericType<List<Profile>>() {
-        });
+        return client
+                .target(uri + "/profiles/search?firstname=" + firstName)
+                .request()
+                .get()
+                .readEntity(new GenericType<List<Profile>>() {
+                });
     }
 
     static List<Profile> getAllProfiles() {
-        ResteasyWebTarget webTarget = client.target(uri + "/profiles");
-        Response response = webTarget.request().get();
-        return response.readEntity(new GenericType<List<Profile>>() {
-        });
+        return client
+                .target(uri + "/profiles")
+                .request(APPLICATION_JSON)
+                .get()
+                .readEntity(new GenericType<List<Profile>>() {
+                });
+
     }
 }
